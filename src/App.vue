10f59fd5c853547ecfe02ec1d14d088714eeb464
Sandro Schmid <saseb.schmid@gmail.com>
2022-03-27 09:19:21 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    Sandro Schmid
+  </header>
+
+  <main>
+    This website is under construction.
+  </main>
+</template>
+
+<style>
+@import "./assets/base.css";
+</style>
